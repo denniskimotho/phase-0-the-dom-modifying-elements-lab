33cdcce450128f5ca5f0a258e5dc76b22f743e7a
denniskimotho <denniskimotho222@gmail.com>
2023-10-06 18:50:55 +0300

    Initial Message

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const element = document.getElementById("main");
+
+element.remove()
+
+const newHeader = document.createElement("h1")
+
+
+newHeader.setAttribute("id","victory")
+newHeader.innerHTML = "Dennis Kimotho is the champion"
+
+document.body.append(newHeader)
